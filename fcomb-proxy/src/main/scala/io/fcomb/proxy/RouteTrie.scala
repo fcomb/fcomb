87c45d2f39984adda6ac6fd4d27fade75848d21c
Timothy Klim <klimtimothy@gmail.com>
2015-08-08 18:04:23 +0300

    chore: small refactoring

--- fcomb-proxy/src/main/scala/io/fcomb/proxy/RouteTrie.scala
+++ fcomb-proxy/src/main/scala/io/fcomb/proxy/RouteTrie.scala
@@ -7,24 +7,24 @@ import scala.collection.mutable.OpenHashMap
 sealed trait RouteKind
 
 @SerialVersionUID(1L)
-case object StaticRoute extends RouteKind // /uri
+private case object StaticRoute extends RouteKind // /uri
 
 @SerialVersionUID(1L)
-case class WildcardRoute( // /*param
+private case class WildcardRoute( // /*param
     name: String
 ) extends RouteKind {
   require(name.nonEmpty, "name can't be empty")
 }
 
 @SerialVersionUID(1L)
-case class ParameterRoute( // /:param
+private case class ParameterRoute( // /:param
     name: String
 ) extends RouteKind {
   require(name.nonEmpty, "name can't be empty")
 }
 
 @SerialVersionUID(1L)
-case class RouteNode[T](
+private[proxy] case class RouteNode[T](
     key:            String,
     value:          Option[T],
     kind:           RouteKind,
@@ -37,7 +37,7 @@ case class RouteNode[T](
   if (kind == ParameterRoute)
     require(key.head != ':', s"Parameter route node must start with prefix symbol ':': $this")
   if (kind == StaticRoute)
-    require(!key.exists(c => c == ':' || c == '*'), s"Static route node must not start with prefix symbol ':' or '*': $this")
+    require(!key.exists(nonStaticPrefix), s"Static route node must not start with prefix symbol ':' or '*': $this")
 
   def get(k: String) =
     getTrie(cleanKey(k)).flatMap(_.value)
@@ -46,13 +46,21 @@ case class RouteNode[T](
   private def cleanKey(k: String) =
     if (k.last != '/') k + '/' else k
 
+  @inline
+  private def nonStaticPrefix(c: Char): Boolean =
+    c == ':' || c == '*'
+
+  @inline
+  private def nonStaticPrefix(k: String): Boolean =
+    nonStaticPrefix(k.head)
+
+  @tailrec
   private def getTrie(k: String): Option[RouteNode[T]] =
     if (k == key) Some(this)
     else if (k.contains(key)) {
       children.get(k(key.length)) match {
-        case Some(c: RouteNode[T]) =>
-          c.getTrie(k.drop(key.length))
-        case _ => None
+        case Some(c: RouteNode[T]) => c.getTrie(k.drop(key.length))
+        case _                     => None
       }
     } else None
 
@@ -62,47 +70,37 @@ case class RouteNode[T](
     val k = cleanKey(kv._1)
     val v = kv._2
 
-    if (k.head == ':' || k.head == '*') addToChildParameter(k, v)
+    if (nonStaticPrefix(k)) addToChildParameter(k, v)
     else {
       if (k == key) RouteNode(k, Some(v), kind, children, childParameter)
       else if (k.startsWith(key)) {
         val newKey = k.drop(key.length)
-        if (newKey.head == ':' || newKey.head == '*')
-          addToChildParameter(newKey, v)
-        else this.copy(
-          children = children += (newKey(0) -> addToChildren(newKey, v))
-        )
+        if (nonStaticPrefix(newKey)) addToChildParameter(newKey, v)
+        else {
+          children += (newKey(0) -> addToChildren(newKey, v))
+          this
+        }
       } else if (key.startsWith(k)) {
         val newKey = key.drop(k.length)
-        RouteNode(
-          k,
-          Some(v),
-          StaticRoute,
-          OpenHashMap[Char, RouteNode[T]](
-            newKey(0) -> RouteNode(newKey, value, kind, children, childParameter)
-          ),
-          None
+        val cc = OpenHashMap(
+          newKey(0) -> RouteNode(newKey, value, kind, children, childParameter)
         )
+        RouteNode(k, Some(v), StaticRoute, cc, None)
       } else {
         val newKey = longestCommonPart(k, key)
         val k1 = key.drop(newKey.length)
         val k2 = k.drop(newKey.length)
-        RouteNode(
-          newKey,
-          None,
-          StaticRoute,
-          OpenHashMap(
-            k1(0) -> RouteNode(k1, value, kind, children, childParameter),
-            k2(0) -> RouteNode(k2, Some(v), StaticRoute)
-          ),
-          None
+        val cc = OpenHashMap(
+          k1(0) -> RouteNode(k1, value, kind, children, childParameter),
+          k2(0) -> RouteNode(k2, Some(v), StaticRoute)
         )
+        RouteNode(newKey, None, StaticRoute, cc, None)
       }
     }
   }
 
   private def addToChildParameter(k: String, v: T) = {
-    require(k.head == ':' || k.head == '*', s"Key '$k' must start with ':' or '*'")
+    require(nonStaticPrefix(k), s"Key '$k' must start with ':' or '*'")
 
     val keyName = k.takeWhile(_ != '/')
     val keyPostfix = k.drop(keyName.length)
@@ -116,28 +114,23 @@ case class RouteNode[T](
         else throw new Exception(s"Conflict on ${n.kind} =!= $route")
       case None => RouteNode(keyName, None, route)
     }
-    val childNodeChildren = childNode.children += (
+    childNode.children += (
       keyPostfix(0) -> childNode.addToChildren(keyPostfix, v)
     )
-    this.copy(
-      childParameter = Some(childNode.copy(
-        children = childNodeChildren
-      ))
-    )
+    this.copy(childParameter = Some(childNode))
   }
 
   private def addToChildren(k: String, v: T): RouteNode[T] =
     children.get(k(0)) match {
       case Some(n) => n + (k -> v)
       case None =>
-        val keyPrefix = k.takeWhile(c => c != ':' && c != '*')
+        val keyPrefix = k.takeWhile(!nonStaticPrefix(_))
         if (keyPrefix.length == k.length)
           RouteNode(keyPrefix, Some(v), StaticRoute)
         else if (keyPrefix.nonEmpty)
           RouteNode(keyPrefix, None, StaticRoute)
             .addToChildParameter(k.drop(keyPrefix.length), v)
-        else
-          addToChildParameter(k, v)
+        else addToChildParameter(k, v)
     }
 
   private def longestCommonPart(a: String, b: String) = {
@@ -150,12 +143,7 @@ case class RouteNode[T](
     ): String = {
       (a.headOption, b.headOption) match {
         case (Some(c1), Some(c2)) if c1 == c2 =>
-          f(
-            a.drop(1),
-            b.drop(1),
-            index + 1,
-            commonPart.append(c1)
-          )
+          f(a.drop(1), b.drop(1), index + 1, commonPart.append(c1))
         case _ => commonPart.toString
       }
     }
@@ -171,8 +159,7 @@ case class RouteNode[T](
     val fullKey = keyPrefix + key
     value.foreach(v => f(fullKey -> v))
     children.foreach {
-      case (_, c: RouteNode[T]) =>
-        c.foreach(f, fullKey)
+      case (_, c: RouteNode[T]) => c.foreach(f, fullKey)
     }
     childParameter.foreach(_.foreach(f, fullKey))
   }
