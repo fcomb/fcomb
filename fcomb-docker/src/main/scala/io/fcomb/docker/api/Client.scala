2d99c7d8a3868464080a1034dce42883b260062f
Timothy Klim <klimtimothy@gmail.com>
2015-11-25 00:50:01 +0300

    chore: change filter format

--- fcomb-docker/src/main/scala/io/fcomb/docker/api/Client.scala
+++ fcomb-docker/src/main/scala/io/fcomb/docker/api/Client.scala
@@ -52,7 +52,7 @@ final class Client(val host: String, val port: Int)(
     limit: Option[Int] = None,
     beforeId: Option[String] = None,
     sinceId: Option[String] = None,
-    filters: Map[String, String] = Map.empty
+    filters: Map[String, List[String]] = Map.empty
   ) = {
     val params = Map(
       "all" -> showAll.toString,
@@ -354,7 +354,7 @@ final class Client(val host: String, val port: Int)(
 
   def images(
     showAll: Boolean = false,
-    filters: Map[String, String] = Map.empty,
+    filters: Map[String, List[String]] = Map.empty,
     withName: Option[String] = None
   ) = {
     val params = Map(
