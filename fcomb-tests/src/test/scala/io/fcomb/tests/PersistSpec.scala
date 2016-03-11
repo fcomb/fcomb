33e7bdb7bbb0a630d6e960b97af94230e31ea643
Timothy Klim <klimtimothy@gmail.com>
2016-03-12 00:01:57 +0300

    chore: persist spec clenaup

--- fcomb-tests/src/test/scala/io/fcomb/tests/PersistSpec.scala
+++ fcomb-tests/src/test/scala/io/fcomb/tests/PersistSpec.scala
@@ -1,47 +1,51 @@
 package io.fcomb.tests
 
 import io.fcomb.Db
-import io.fcomb.persist._
+import io.fcomb.{persist ⇒ P}
 import scala.concurrent.Await
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration.Duration
-import org.scalatest._ // {BeforeAndAfterAll, BeforeAndAfterEach}
+import org.scalatest.{BeforeAndAfterAll, BeforeAndAfterEach, Suite}
 import org.slf4j.LoggerFactory
 
 private[this] final object PersistSpec {
   lazy val logger = LoggerFactory.getLogger(getClass)
 
-  lazy val initDbOnce = {
+  lazy val initDbOnlyOnce = {
     logger.debug("Clean db schema")
     val conn = Db.db.createSession().conn
     val st = conn.prepareStatement("""
       DROP SCHEMA IF EXISTS public CASCADE;
       CREATE SCHEMA public;
     """)
-    st.executeUpdate()
-    conn.close()
+    try st.executeUpdate()
+    finally conn.close()
     logger.debug("Migrate db schema")
     Await.result(Db.migrate(), Duration.Inf)
   }
+
+  val tables = Set(
+    P.docker.distribution.Image.table,
+    P.User.table
+  )
+
+  val truncateQuery =
+    tables
+      .map(t ⇒ s"TRUNCATE ${t.baseTableRow.tableName} CASCADE")
+      .mkString(";\n")
 }
 
 trait PersistSpec extends BeforeAndAfterAll with BeforeAndAfterEach { this: Suite ⇒
   lazy val logger = LoggerFactory.getLogger(getClass)
 
   override def beforeAll(): Unit =
-    PersistSpec.initDbOnce
+    PersistSpec.initDbOnlyOnce
 
   override def beforeEach(): Unit = {
     logger.debug("Truncate tables")
     val conn = Db.db.createSession().conn
-    val tables = Set(
-      docker.distribution.Image.table,
-      User.table
-    )
-    val st = conn.prepareStatement(tables.map { table ⇒
-      s"TRUNCATE ${table.baseTableRow.tableName} CASCADE"
-    }.mkString(";\n"))
-    st.executeUpdate()
-    conn.close()
+    val st = conn.prepareStatement(PersistSpec.truncateQuery)
+    try st.executeUpdate()
+    finally conn.close()
   }
 }
