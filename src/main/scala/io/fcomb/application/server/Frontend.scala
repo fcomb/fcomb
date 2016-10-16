54d6041ed128487539b9ea0e8cb60270b2714f70
Timothy Klim <klimtimothy@gmail.com>
2016-10-17 01:56:22 +0300

    fix: frontend assets

--- src/main/scala/io/fcomb/application/server/Frontend.scala
+++ src/main/scala/io/fcomb/application/server/Frontend.scala
@@ -4,12 +4,9 @@ import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
 
 object Frontend {
-  val routes: Route = {
+  val routes: Route =
     // format: OFF
-    encodeResponse {
-      pathEndOrSingleSlash(get(getFromResource("public/index.html"))) ~
-      get(getFromResourceDirectory("public"))
-    }
+    pathEndOrSingleSlash(get(getFromResource("public/index.html"))) ~
+    get(getFromResourceDirectory("public"))
     // format: ON
-  }
 }
