652691218868a896f4ffb3f4f012f35859c7e7db
Timothy Klim <klimtimothy@gmail.com>
2016-11-02 18:59:15 +0300

    fix: group members spec

--- fcomb-tests/src/test/scala/io/fcomb/server/api/organization/GroupsHandlerSpec.scala
+++ fcomb-tests/src/test/scala/io/fcomb/server/api/organization/GroupsHandlerSpec.scala
@@ -22,7 +22,9 @@ import akka.http.scaladsl.testkit.ScalatestRouteTest
 import io.fcomb.json.models.errors.Formats.decodeErrors
 import io.fcomb.json.rpc.Formats._
 import io.fcomb.models.acl.Role
+import io.fcomb.models.common.Slug
 import io.fcomb.models.errors.Errors
+import io.fcomb.persist.OrganizationGroupsRepo
 import io.fcomb.rpc.helpers.UserHelpers
 import io.fcomb.rpc._
 import io.fcomb.server.Api
@@ -126,12 +128,15 @@ final class GroupsHandlerSpec
       }
     }
 
-    "return a list of suggested users" in {
+    "return a list of suggested users which not in group" in {
       val (user, org, user1, user2) = Fixtures.await(for {
-        user  <- UsersFixture.create()
-        org   <- OrganizationsFixture.create(userId = user.getId())
-        user1 <- UsersFixture.create(email = "user1@fcomb.io", username = "user1")
-        user2 <- UsersFixture.create(email = "user2@fcomb.io", username = "user2")
+        user        <- UsersFixture.create()
+        org         <- OrganizationsFixture.create(userId = user.getId())
+        user1       <- UsersFixture.create(email = "user1@fcomb.io", username = "user1")
+        user2       <- UsersFixture.create(email = "user2@fcomb.io", username = "user2")
+        user3       <- UsersFixture.create(email = "user3@fcomb.io", username = "user3")
+        Some(group) <- OrganizationGroupsRepo.findBySlug(org.getId(), Slug.Name("admins"))
+        _           <- OrganizationGroupUsersFixture.create(group.getId(), user3.getId())
       } yield (user, org, user1, user2))
 
       Get(s"/v1/organizations/${org.name}/groups/admins/suggestions/members?q=user") ~> authenticate(
